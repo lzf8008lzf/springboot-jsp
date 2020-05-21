d5616624a715a0666dd83d7445d5f4e6b660b0b5
LiZhaofu <lzf8008lzf@163.com>
2020-05-21 10:58:32 +0800

    springboot集成jsp及freemarker

--- src/main/java/com/enjoy/StartApplication.java
+++ src/main/java/com/enjoy/StartApplication.java
@@ -2,9 +2,24 @@ package com.enjoy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @SpringBootApplication
 public class StartApplication {
+
+    @Bean
+    public FreeMarkerViewResolver getFreeMarkerViewResolver() {
+        FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver();
+        freeMarkerViewResolver.setCache(true);
+        freeMarkerViewResolver.setPrefix("");
+        freeMarkerViewResolver.setSuffix(".ftl");
+        freeMarkerViewResolver.setRequestContextAttribute("request");
+        freeMarkerViewResolver.setOrder(0);
+        freeMarkerViewResolver.setContentType("text/html;charset=UTF-8");
+
+        return freeMarkerViewResolver;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
--- src/main/java/com/enjoy/controller/HelloController.java
+++ src/main/java/com/enjoy/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.enjoy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
         model.addAttribute("name", name);
         return "hello";
+    }
+
+    @RequestMapping("/free")
+    public String index(ModelMap map) {
+        map.addAttribute("host","https://github.com/Inverseli/");
+        return "index";
     }
 }
